bd5db28125691da2e1448f8895f4bb8342e9b629
RickvanHam <428294@student.fontys.nl>
2020-05-20 13:50:22 +0200

    Add project files.

---
+++ Controllers/ApplicationUserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StoomWeb_API.Models;
+
+namespace StoomWeb_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signinManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signinManager) 
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        //Post : api/ApplicationUser/Register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationuser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationuser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoomWeb_API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
